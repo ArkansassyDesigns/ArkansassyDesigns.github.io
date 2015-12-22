1202b007735b875d6b2c3c70ed7ee4db5b876c58
ross <rosstafarian1@gmail.com>
2015-12-22 01:47:06 -0600

    Initial commit. Product page and comments

---
+++ app/components/Navigation.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Router from 'react-router'
+
+class Navigation extends React.Component {
+    getRef(ref) {
+        this.productNameRef = ref
+    }
+
+    handleSubmit() {
+        const productName = this.productNameRef.value
+        this.productNameRef.value = ''
+        this.props.history.pushState(null, `/product/${productName}`)
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">Arkansassy Designs</a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li className="active"><a href="#">Products <span className="sr-only">(current)</span></a></li>
+                        </ul>
+                        <form className="navbar-form navbar-right" role="search" onSubmit={() => this.handleSubmit()}>
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Search" style={{marginRight: 10}} ref={(ref) => this.getRef(ref)} />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Search Products</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+Navigation.PropTypes = {
+    history: React.PropTypes.object.isRequired
+}
+
+export default Navigation
